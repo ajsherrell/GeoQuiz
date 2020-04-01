1775de2159a864c8e5bcad7ff9350fb4ae7383c2
ajsherrell <ajsherrell.ajacks@gmail.com>
2020-04-01 15:50:18 -0400

    modify gitIgnore

--- app/src/main/java/com/ajsherrell/geoquiz/MainActivity.kt
+++ app/src/main/java/com/ajsherrell/geoquiz/MainActivity.kt
@@ -2,8 +2,6 @@ package com.ajsherrell.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
-import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
